4a144d8c5fe71d83ffa3d0690d49448784fc2076
KarimE146 <karim666the@gmail.com>
2024-08-03 00:38:09 -0400

    helper funcs done

--- pantry_traker/app/page.js
+++ pantry_traker/app/page.js
@@ -1,5 +1,74 @@
+'use client'
 import Image from "next/image";
+import {useStates, useEffect} from 'react'
+import { firestore } from "@/firebase";
+import { Box, Typography } from "@mui/material";
+import { collection, deleteDoc, onSnapshot, query, setDoc } from "firebase/firestore";
 
 export default function Home() {
-  return <>Hello World</>
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  const updateInventory = async() =>{
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) =>{
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  const removeItem = async (item) =>{
+    const docReft = doc(collection(firestore, 'inventory'),item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if(quantity ===1){
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, {quantity: quantity -1})
+      }
+    }
+    await updateInventory()
+  } 
+
+  const addItem = async (item) =>{
+    const docReft = doc(collection(firestore, 'inventory'),item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity +1})
+    } else {
+      await setDoc(docRef, {quantity:1})
+    }
+    await updateInventory()
+  } 
+
+
+  useEffect(()=>{
+    updateInventory()
+  }, [])
+
+  return( 
+    <Box>
+      <Typography variant="h1">Inventory management</Typography>
+      {
+        inventory.forEach((item)=>{
+          return(
+          <Box>
+          {item.name}
+          {item.count}
+          </Box>
+          )
+        })
+      }
+    </Box>
+  )
 }
